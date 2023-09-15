Wrong calculation process

algorithm/base.py
@@ -36,7 +36,6 @@ class BaseClient(ABC):
         self.local_model = deepcopy(model).to(self.device)
         self.personal_model = deepcopy(model).to(self.device)
 
-        # Load and preprocess client-specific data
         data = torch.load(f'./data/{dataset}/data/client{client_id}/x.pt')
         labels = torch.load(f'./data/{dataset}/data/client{client_id}/y.pt')
         train_size = int(0.8 * len(data))
@@ -47,7 +46,6 @@ class BaseClient(ABC):
         self.test_data = test_dataset.dataset.tensors[0]
         self.test_labels = test_dataset.dataset.tensors[1]
 
-        # Create data loaders for training and testing
         self.train_dataloader = DataLoader(train_dataset, batch_size=local_batch_size, shuffle=True)
         self.test_dataloader = DataLoader(test_dataset, batch_size=local_batch_size, shuffle=False)
         self.train_full_dataloader = DataLoader(train_dataset, batch_size=len(train_dataset), shuffle=False)
@@ -74,6 +72,19 @@ class BaseClient(ABC):
         This method should be implemented in subclasses to define the specific training logic for the client.
         During local training, the client's local model should learn from its local dataset for a certain number
         of epochs (specified by self.local_epochs).
+        """
+        pass
+
+    @abstractclassmethod
+    def step_decay(self) -> None:
+        """
+        Abstract method for applying learning rate decay specific to the client.
+
+        Args:
+            - epoch (int): Current epoch during training.
+
+        This method should be implemented in subclasses to define the client-specific learning rate decay logic.
+        It allows clients to apply custom learning rate decay schedules.
         """
         pass
 
@@ -153,8 +164,9 @@ class BaseClient(ABC):
         self.set_model(params_backup)
         return (correct_counts / total_samples), total_samples
 
+
 class BaseServer(ABC):
-    def __init__(self, algorithm: str, dataset: str, device: str, model: nn.Module, global_learning_rate: float,
+    def __init__(self, algorithm: str, dataset: str, device: str, model: nn.Module, lr_global: float,
                  selection_ratio: float, round: int):
         """
         Initializes a server object with the following parameters.
@@ -164,7 +176,7 @@ class BaseServer(ABC):
             - dataset (str): Name of the dataset used for federated learning.
             - device (str): Specifies the device on which the server runs, e.g., "cpu" or "cuda:0".
             - model (nn.Module): The global model used in federated learning, a PyTorch neural network model.
-            - global_learning_rate (float): Learning rate for global model updates.
+            - lr_global (float): Learning rate for global model updates.
             - selection_ratio (float): Ratio of selected clients for each communication round.
             - round (int): Current communication round number.
 
@@ -181,7 +193,8 @@ class BaseServer(ABC):
         self.dataset: str = dataset
         self.device: str = device
         self.global_model = model.to(self.device)
-        self.global_learning_rate: float = global_learning_rate
+        self.lr_global: float = lr_global
+        self.lr_global_init: float = lr_global
         self.clients: list[BaseClient] = []
         self.selection_ratio: float = selection_ratio
         self.round: int = round
@@ -252,6 +265,19 @@ class BaseServer(ABC):
 
         This method should be implemented in subclasses to define the logic for updating
         the global model based on the aggregated client updates obtained during global training.
+        """
+        pass
+
+    @abstractclassmethod
+    def global_decay(self) -> None:
+        """
+        Apply global learning rate decay for the server and the clients.
+
+        Args:
+            - epoch (int): Current epoch during training.
+
+        This method applies learning rate decay globally. It can be used to adjust the global
+        learning rate of both the server's and the clients'.
         """
         pass
 

algorithm/pFMeMo.py
@@ -30,6 +30,8 @@ class pFMeMoClient(BaseClient):
         self.delta: float = delta
         self.lr_p: float = lr_p
         self.lr_l: float = lr_l
+        self.lr_p_init: float = lr_p
+        self.lr_l_init: float = lr_l
         self.local_update: list[torch.Tensor] = list()
 
     def calculate_grad_per(self, batch_idx: int) -> list[torch.Tensor]:
@@ -85,6 +87,7 @@ class pFMeMoClient(BaseClient):
         return grads_local
 
     def update_per_model(self, batch_idx: int) -> None:
+        # TODO: Adjust the calculation process.
         """
         Update the personal model with early stopping.
 
@@ -96,7 +99,8 @@ class pFMeMoClient(BaseClient):
         until the norm of the gradient falls below the given threshold (delta).
         """
         count = 0
-        while True:
+        norms = []
+        while count < 1e4:
             count += 1
             grad_h = self.calculate_grad_local(batch_idx)
             grad_h_cat: list[torch.Tensor] = []
@@ -105,11 +109,14 @@ class pFMeMoClient(BaseClient):
             grad_h_cat = torch.cat(grad_h_cat)
 
             norm = torch.norm(grad_h_cat)
-            if norm ** 2 <= self.delta ** 2:
+            norms.append(norm)
+            if norm <= self.delta:
                 break
 
             for param_per, grad in zip(self.personal_model.parameters(), grad_h):
                 param_per.data -= self.lr_p * grad
+        # print(f"count = {count} norm = {norm}")
+        print(f"norm = {torch.mean(torch.tensor(norms)).item()}")
 
     def update_local_model(self) -> None:
         """
@@ -133,6 +140,7 @@ class pFMeMoClient(BaseClient):
         updates the personal model with early stopping and incorporates regularization terms.
         After training, it calculates the updates made to the local model and stores them.
         """
+        print(f"# Client {self.client_id} Training...")
         self.local_update.clear()
         for i in range(self.local_epochs):
             self.update_per_model(batch_idx=i)
@@ -152,6 +160,20 @@ class pFMeMoClient(BaseClient):
         the local model and the global model.
         """
         return self.local_update
+    
+    def step_decay(self, epoch: int) -> None:
+        """
+        Apply exponential decay to self.delta, self.lr_p, and self.lr_l.
+
+        Args:
+            - epoch (int): Current epoch during training.
+
+        This method applies exponential decay to self.delta, self.lr_p, and self.lr_l based on the current epoch.
+        """
+        decay_factor = 0.95
+        self.lr_p = self.lr_p_init * (decay_factor ** epoch)
+        self.lr_l = self.lr_l_init * (decay_factor ** epoch)
+
 
 class pFMeMoServer(BaseServer):
     def __init__(self, algorithm, dataset, device, model, lr_g, user_selection_ratio, round):
@@ -171,6 +193,7 @@ class pFMeMoServer(BaseServer):
         device, global model, learning rate, client selection ratio, and communication rounds.
         """
         super().__init__(algorithm, dataset, device, model, lr_g, user_selection_ratio, round)
+        self.clients: list[pFMeMoClient] = []
     
     def calculate_weights(self) -> tuple[list[list[torch.Tensor]], torch.Tensor]:
         """
@@ -240,12 +263,27 @@ class pFMeMoServer(BaseServer):
                 global_update += client_update * weight
         
         for global_param, global_update in zip(self.global_model.parameters(), global_updates):
-            global_param.data += self.global_learning_rate * global_update
+            global_param.data += self.lr_global * global_update
         
         global_updates_flatten: list[torch.Tensor] = []
         for global_update in global_updates:
             global_updates_flatten.append(global_update.flatten())
         print(f"global update = {torch.mean(torch.cat(global_updates_flatten)).item()}")
+    
+    def global_decay(self, epoch: int) -> None:
+        """
+        Apply global learning rate decay for the server and the clients.
+
+        Args:
+            - epoch (int): Current epoch during training.
+
+        This method applies learning rate decay globally. It can be used to adjust the global
+        learning rate of both the server's and the clients'.
+        """
+        decay_factor = 0.95
+        self.lr_global = self.lr_global_init * (decay_factor ** epoch)
+        for client in self.clients:
+            client.step_decay(epoch)
 
     def global_train(self, save_name_addition: str) -> None:
         """
@@ -259,7 +297,7 @@ class pFMeMoServer(BaseServer):
         model performance, and saves the results.
         """
         for i in range(self.round):
-            print(f"\n***** ROUND {i} *****")
+            print(f"\n**********  ROUND {i}  **********")
             self.send_global_model()
             for client in self.clients:
                 client.local_train()
@@ -267,4 +305,7 @@ class pFMeMoServer(BaseServer):
             self.model_evaluate()
             self.model_per_evaluate()
             self.model_global_test()
+            if i % 10 == 0:
+                self.global_decay(i)
         self.save_result(save_name_addition)
+        self.save_model(save_name_addition)