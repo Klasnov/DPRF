24c57cffe266c9e52d9f5e6beeb09bd2f5c9f386
Yanming Shao <klasnovshao@gmail.com>
2023-11-12 01:53:06 +0800

    improve model of CIFAR-10

--- utils/util_models.py
+++ utils/util_models.py
@@ -20,21 +20,28 @@ class Cifar10_Model(nn.Module):
     def __init__(self):
         super(Cifar10_Model, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.norm = nn.BatchNorm2d(32)
-        self.fc1 = nn.Linear(32 * 14 * 14, 2048)
-        self.fc2 = nn.Linear(2048, 512)
-        self.fc3 = nn.Linear(512, 10)
+        self.fc1 = nn.Linear(32 * 6 * 6, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 64)
+        self.fc4 = nn.Linear(64, 10)
+        self.drop = nn.Dropout(p=0.3)
     
     def forward(self, x: torch.Tensor):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
         x = self.norm(self.pool(x))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.drop(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 class Emnist_Model(nn.Module):
